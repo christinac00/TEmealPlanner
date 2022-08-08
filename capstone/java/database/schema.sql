ac37b118b963755e08a02868880b2c02e3f45962
Christina Ngo <ngo.christina38@gmail.com>
2022-08-08 16:55:08 -0400

    starting create tables

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -10,4 +10,29 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+
+CREATE TABLE plan (
+    plan_id SERIAL,
+    name varchar (50),
+    day_of_week varchar (20),
+    CONSTRAINT PK_plan PRIMARY KEY (plan_id)
+);
+
+CREATE TABLE recipe_plan (
+    plan_id int,
+    recipe_id int,
+    time_of_day varchar (20),
+    CONSTRAINT 
+);
+
+CREATE TABLE recipe (
+    recipe_id SERIAL,
+    name varchar,
+    ingredient int,
+    diet_type varchar,
+    instruction text
+);
+
+
+
 COMMIT TRANSACTION;
