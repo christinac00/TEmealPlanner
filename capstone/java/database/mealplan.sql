0004dc0e430e5a83a7682a1bc72f85d930b453d9
Christina Ngo <ngo.christina38@gmail.com>
2022-08-09 17:06:45 -0400

    added recipe_ingredient table

--- capstone/java/database/mealplan.sql
+++ capstone/java/database/mealplan.sql
@@ -7,16 +7,9 @@ INSERT INTO ingredient (name, quantity, unit, category) VALUES ('shrimp', 1, 'lb
 INSERT INTO ingredient (name, quantity, unit, category) VALUES ('romaine lettuce', 1, 'bowl', 'vegetable');
 INSERT INTO ingredient (name, quantity, unit, category) VALUES ('caesar dressing', 1, 'tbsp', 'condiment');
 
-COMMIT TRANSACTION;
-
-
-
-BEGIN TRANSACTION;
-
-INSERT INTO recipe (name, ingredient_id, diet_type, instruction) VALUES ('Caesar Salad', (SELECT ingredient_id FROM ingredient WHERE name='romaine lettuce'), 'vegetarian', 'shake it up');
-INSERT INTO recipe (name, ingredient_id, diet_type, instruction) VALUES ('Caesar Salad', (SELECT ingredient_id FROM ingredient WHERE name='caesar dressing'), 'vegetarian', 'shake it up');
 
 INSERT INTO plan (name, day_of_week) VALUES ('Monday Lunch', 'Monday');
+
 
 COMMIT TRANSACTION;
 
