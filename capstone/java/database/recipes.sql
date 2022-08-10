cda3208033a3e407b4a76e29e29812550d3cb00f
Jamoni Harris <harrisjamoni@gmail.com>
2022-08-10 15:28:05 -0400

    Populated ingredient, recipe, and recipe_ingredient tables

---
+++ capstone/java/database/recipes.sql
@@ -0,0 +1,16 @@
+START TRANSACTION;
+--INSERT recipes into table
+--Breakfast Smoothies
+INSERT INTO recipe(name, instructions) VALUES ('Breakfast Smoothies','Step 1: Gradually add all ingredients to the jar of a blender; puree until smooth. Serve.');
+
+--Black Bean Tacos
+INSERT INTO recipe(name, instructions) VALUES ('Black Bean Tacos','Step 1: Preheat oven to 325 degrees F. Step 2: Mash half of the beans in a large bowl. Add whole beans, cumin and garlic powder; stir to combine. Fill each taco shell with about 1/3 cup of the bean mixture. Top with cheese. Place the filled tacos on a baking sheet and bake until the beans are hot and the cheese is melted, 12 to 15 minutes.');
+
+--Southern Sherried Shrimp
+INSERT INTO recipe(name, instructions) VALUES ('Southern Sherried Shrimp','Step 1: In a frying pan or large skillet, melt butter over medium-high heat until foamy but not browned.
+');
+
+--Peanut Butter Banana Pops
+INSERT INTO recipe(name, instructions) VALUES ('Peanut Butter Banana Pops','Step 1: Peel the bananas and cut them into coins. It does not matter what shape or size the pieces are in, as long as they are chopped up into evenly sized and somewhat small pieces. Put the bananas in a freezer-safe glass bowl or bag. Freeze for at least 2 hours, but ideally overnight.
+');
+COMMIT;
