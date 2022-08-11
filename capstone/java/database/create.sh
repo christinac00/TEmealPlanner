50cddc17d3e912e2d370c9d8503a4b7a6c10dfb8
Christina Ngo <ngo.christina38@gmail.com>
2022-08-10 23:24:01 -0400

    working get User Recipes

--- capstone/java/database/create.sh
+++ capstone/java/database/create.sh
@@ -6,5 +6,8 @@ psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
-#psql -U postgres -d $DATABASE -f "$BASEDIR/mealplan.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/recipes.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/ingredients.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/recipe-ingredient.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/user-recipe.sql"
