2b8ba6fc0ae0b2b4aeff0a242481fca8210a3591
Jamoni Harris <harrisjamoni@gmail.com>
2022-08-12 12:12:20 -0400

    MealPlan Model and Dao created, Jdbc in progress..

---
+++ capstone/java/src/main/java/com/techelevator/dao/MealPlanDao.java
@@ -0,0 +1,22 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.MealPlan;
+
+import java.util.List;
+
+public interface MealPlanDao {
+    //Lists all meal plans
+    List<MealPlan> list();
+
+    //create a new meal plan
+    MealPlan createMealPlan(MealPlan newMealPlan);
+
+    //get a meal plan by name
+    MealPlan getMealPlanByName(String planName);
+
+    //update a meal plan
+    void updateMealPlan(MealPlan updatedMealPlan);
+
+    //delete a meal plan
+    void deleteMealPlan(int planId);
+}
