f03ba4f3eeeecde24ede65da7abea5c844bbbbb5
james schwartz <jschwartz374@gmail.com>
2022-08-14 14:36:41 -0400

    mealplan JMS

--- capstone/vue/src/components/MealPlans.vue
+++ capstone/vue/src/components/MealPlans.vue
@@ -1,11 +1,14 @@
 <template>
- 
+ <div class="my-plans">
   <div class="loading" v-if="isLoading">
     <img src="../assets/your-food-is-being-prepared.gif" />
     </div>
-    
-    <meal-card v-for="plan in my-meal-plans" v-bind:key="plan.name"
+    <div class="plans-header">
+        <h1>My Meal Plan Library</h1>
+    </div>
+    <meal-card v-for="plan in myMealPlans" v-bind:key="plan.name"
       v-bind:plan="plan" />
+</div>
 </template>
 
 <script>
