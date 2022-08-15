a42ef3ffa169faa302a5879c24d046a1b01f3a33
Michael Kong-Quee <mikekongquee@gmail.com>
2022-08-15 11:46:24 -0400

    Vue and Java changes

--- capstone/vue/src/views/MealPlan.vue
+++ capstone/vue/src/views/MealPlan.vue
@@ -2,9 +2,9 @@
   <div>
      <meal-plans />
       <add-plan /> 
-      <router-link tag="add meal plan" :to="{name:'add-plan',params:{userId: $route.params.id}}">
+      <!-- <router-link tag="div" :to="{name:'add-plan',params:{userId: $route.params.id}}">
           
-      </router-link>
+      </router-link> -->
 
   </div>
 </template>
